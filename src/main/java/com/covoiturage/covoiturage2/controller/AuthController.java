2b223ecd3d58eb2a03e59647b9ff58dd2f4c1105
waelghribi <waelghribi49@gmail.com>
2025-04-24 10:30:40 +0100

    last version

--- src/main/java/com/covoiturage/covoiturage2/controller/AuthController.java
+++ src/main/java/com/covoiturage/covoiturage2/controller/AuthController.java
@@ -8,16 +8,14 @@ import com.covoiturage.covoiturage2.security.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
