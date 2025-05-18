24c4887bb8f05da8f724bbc1eec79b310c2d13cf
fedi <fedibenabdesslem46@gmail.com>
2025-05-18 10:48:23 +0100

    update du controller reservation

--- src/main/java/com/covoiturage/covoiturage2/controller/ReservationController.java
+++ src/main/java/com/covoiturage/covoiturage2/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
